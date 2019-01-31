f598b78c7fc96e9b8e0e6dc395465f62997a3922
jkebx22 <42923140+jkebx22@users.noreply.github.com>
2019-01-30 18:30:14 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	float[] theVals = {10.0f, 20.0f, 15.0f};
+
+	float sum = 0.0f;
+
+	for (int i = 0; i < theVals.length; i++)
+	    sum += theVals[i];
+
+	System.out.println(sum);
+    }
+}
